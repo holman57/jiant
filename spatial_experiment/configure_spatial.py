6b74647d70ad76b1d40dc22c45b857c2a1175551
Luke Holman <lukeholman@my.unt.edu>
2021-06-03 17:31:43 -0500

    rollback path refactor

--- spatial_experiment/configure_spatial.py
+++ spatial_experiment/configure_spatial.py
@@ -4,8 +4,8 @@ import jiant.utils.display as display
 import os
 
 jiant_run_config = configurator.SimpleAPIMultiTaskConfigurator(
-    task_config_base_path="./spatial_experiment",
-    task_cache_base_path="./spatial_experiment/cache",
+    task_config_base_path="./tasks/configs",
+    task_cache_base_path="./cache",
     train_task_name_list=["spatial"],
     val_task_name_list=["spatial"],
     train_batch_size=8,
@@ -14,8 +14,6 @@ jiant_run_config = configurator.SimpleAPIMultiTaskConfigurator(
     num_gpus=1,
 ).create_config()
 
-os.makedirs("./spatial_experiment/run_configs/", exist_ok=True)
-py_io.write_json(jiant_run_config, "./spatial_experiment/run_configs/spatial_run_config.json")
+os.makedirs("./run_configs/", exist_ok=True)
+py_io.write_json(jiant_run_config, "./run_configs/spatial_run_config.json")
 display.show_json(jiant_run_config)
-
-
